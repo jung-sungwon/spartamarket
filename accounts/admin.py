7db21035ed7cd2127bafcb38a020175a7cd275eb
jung-sungwon <pompous89@gmail.com>
2024-09-05 14:57:51 +0900

    admin page 회원가입 기능 구현후 테스트 편의성 확보 위해 어드민 페이지 활성화

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+# users/admin.py
+from django.contrib import admin
+from .models import User
+
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'birthday', 'is_staff')
+    search_fields = ('username', 'email')
